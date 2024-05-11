9688cc6f06005aa2f935da31d78e06aaaf2108a1
green. <41323182+gweeeen@users.noreply.github.com>
2024-05-11 15:52:35 +0200

    beta version

--- raidguard_content_protection.py
+++ raidguard_content_protection.py
@@ -16,11 +16,242 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-async def check(bot):
-    """If you need certain code to be ran before unload/reload,
-    write them here.
+import discord
+import revolt
+import guilded
+import time
+import math
+import re
+from tld import get_tld
+from utils import rapidphish
 
-    shutdown must be set to true and utils must include
-    [plugin_id]_check.py."""
+# 0: ignore
+# 1: watch
+# 2: temp
 
-    return
+config = {
+    'constant': 9600,
+    'invites': 2,
+    'rapidphish': 2,
+    'raid': 1,
+    'rapidphish_whitelist': [
+
+    ]
+}
+
+def findurl(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)
+    return [x[0] for x in url]
+
+
+def bypass_killer(string):
+    if not [*string][len(string) - 1].isalnum():
+        return string[:-1]
+    else:
+        raise RuntimeError()
+
+class RaidBan:
+    def __init__(self, identifier, bantype=0, debug=True, frequency=1, constant=9600):
+        self.identifier = str(identifier) # Either an ID or message content
+        self.bantype = bantype # 0: Watchban, 1: Tempban. Watchban lets the content through initially
+        self.frequency = frequency # Frequency of content
+        self.constant = constant # Constant used for raid detection. Lower is more sensitive
+        self.time = round(time.time()) # Time when ban occurred
+        self.duration = 600 # Duration of ban in seconds. Base is 600
+        self.expire = round(time.time()) + self.duration # Expire time
+        self.debug = debug # Debug raidban
+        self.involved = {} # List of involved users (format: {userid: [message_ids]})
+        self.banned = False # If action has been taken or not
+
+    def is_banned(self):
+        if self.expire < time.time():
+            return False or self.banned
+        return True
+
+    def increment(self,count=1):
+        if self.banned:
+            raise RuntimeError()
+        self.frequency += count
+        t = math.ceil((round(time.time())-self.time)/60)
+        i = self.frequency
+        threshold = round(self.constant*t/i)
+        prevd = self.duration
+        self.duration = self.duration * 2
+        diff = self.duration - prevd
+        self.expire += diff
+        self.banned = self.duration > threshold
+        return self.duration > threshold or self.bantype == 1
+
+
+raidbans = []
+
+async def find_raidban(identifier):
+    offset = 0
+    for index in range(len(raidbans)):
+        raidban = raidbans[index-offset]
+        if raidban.expire >= time.time():
+            raidbans.pop(index-offset)
+            offset += 1
+        if raidban.identifier==identifier:
+            return raidban
+    return None
+
+async def scan(message: discord.Message or revolt.Message or guilded.Message):
+    """Message scan logic"""
+
+    global raidbans
+
+    # Example ban entry in target:
+    # {'1187093090415149056': 0} (this is a permanent ban)
+    # {'1187093090415149056': 20} (this is a 20 second ban)
+    response = {
+        'unsafe': False,
+        'description': 'No suspicious content found',
+        'target': {},
+        'delete': []
+    }
+
+    invite = False
+    phishing = False
+    raid = False
+
+    # Prevent message tampering
+    urls = findurl(message.content)
+    filtered = message.content.replace('\\', '')
+    for url in urls:
+        filtered = filtered.replace(url, '', 1)
+    for word in filtered.split():
+        # kill hyperlinks :woke:
+        if '](' in word:
+            # likely hyperlink, lets kill it
+            if word.startswith('['):
+                word = word[1:]
+            if word.endswith(')'):
+                word = word[:-1]
+            word = word.replace(')[', ' ')
+            words = word.split()
+            found = False
+            for word2 in words:
+                words2 = word2.replace('](', ' ').split()
+                for word3 in words2:
+                    if '.' in word3:
+                        if not word3.startswith('http://') or not word3.startswith('https://'):
+                            word3 = 'http://' + word3
+                        while True:
+                            try:
+                                word3 = bypass_killer(word3)
+                            except:
+                                break
+                        if len(word3.split('.')) == 1:
+                            continue
+                        else:
+                            if word3.split('.')[1] == '':
+                                continue
+                        try:
+                            get_tld(word3.lower(), fix_protocol=True)
+                            if '](' in word3.lower():
+                                word3 = word3.replace('](', ' ', 1).split()[0]
+                            urls.append(word3.lower())
+                            found = True
+                        except:
+                            pass
+
+            if found:
+                # successful link detection from hyperlink yippee
+                continue
+        if '.' in word:
+            while True:
+                try:
+                    word = bypass_killer(word)
+                except:
+                    break
+            if len(word.split('.')) == 1:
+                continue
+            else:
+                if word.split('.')[1] == '':
+                    continue
+            try:
+                get_tld(word.lower(), fix_protocol=True)
+                if '](' in word.lower():
+                    word = word.replace('](', ' ', 1).split()[0]
+                urls.append(word.lower())
+            except:
+                pass
+
+    key = 0
+    for url in urls:
+        url = url.lower()
+        urls[key] = url
+        if not url.startswith('http://') and not url.startswith('https://'):
+            urls[key] = f'http://{url}'
+        if '](' in url:
+            urls[key] = url.replace('](', ' ', 1).split()[0]
+        if ('discord.gg/' in url or 'discord.com/invite/' in url or 'discordapp.com/invite/' in url or
+                'discord.gg/' in message.content or 'discord.com/invite/' in message.content or
+                'discordapp.com/invite/' in message.content):
+            invite = True
+        key = key + 1
+
+    if len(urls) > 0:
+        rpresults = rapidphish.compare_urls(urls, 0.85)
+        phishing = rpresults.final_verdict == 'safe'
+
+    chars = ''.join(message.content.split())  # remove all whitespace
+    upper_percent = sum(i.isupper() for i in chars) / len(chars)
+
+    multi = 1
+
+    for line in message.content.split('\n'):
+        if line.startswith('# '):
+            multi *= 4
+        elif line.startswith('## '):
+            multi *= 2
+
+    raid = (upper_percent > 0.5 and len(message.content) > 4 or len(urls) > 0 or
+            round(len(message.content) * multi) > 200)
+
+    punishment = 0
+    if invite:
+        punishment = config['invites']
+    elif phishing:
+        punishment = config['rapidphish']
+    elif raid:
+        punishment = config['raid']
+
+    if punishment==0:
+        pass
+    else:
+        raidban = await find_raidban(f'{message.author.id}' if punishment==0 else message.content)
+        if not raidban:
+            raidban = RaidBan(
+                identifier=f'{message.author.id}' if punishment==0 else message.content,
+                bantype=punishment-1,
+                constant=config['constant']
+            )
+            raidbans.append(raidban)
+        if not f'{message.author.id}' in list(raidban.involved.keys()):
+            raidban.update({f'{message.author.id}': []})
+        raidban[f'{message.author.id}'].append(message.id)
+        toban = raidban.increment()
+        if punishment==2:
+            response['unsafe'] = True
+            response['description'] = f'RaidGuard configured to temporary ban'
+            response['target'] = {
+                f'{message.author.id}': raidban.duration,
+            }
+            response['delete'] = [message.id]
+        if toban:
+            toban = {}
+            todelete = []
+            for user in raidban.involved:
+                toban.update({user:0})
+                todelete.append(raidban.involved[user])
+
+            response['unsafe'] = True
+            response['description'] = f'RaidGuard threshold passed ({raidban.duration}/{config["threshold"]})'
+            response['target'] = toban
+            response['delete'] = todelete
+
+    return response
+
